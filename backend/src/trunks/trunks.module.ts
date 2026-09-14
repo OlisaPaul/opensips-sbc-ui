359686008ec48cce0f0d7beb7a5bf4c28075804f
OlisaPaul <odirahchukwumma28@gmail.com>
2026-09-14 08:57:27 +0100

    Add audit and MI modules to trunks module

--- backend/src/trunks/trunks.module.ts
+++ backend/src/trunks/trunks.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { AuditModule } from '../audit/audit.module';
+import { MiModule } from '../mi/mi.module';
 import { CredentialService } from './credential.service';
 import { TrunkPlannerService } from './trunk-planner.service';
 import { TrunksController } from './trunks.controller';
 import { TrunksService } from './trunks.service';
 
 @Module({
+  imports: [AuditModule, MiModule],
   controllers: [TrunksController],
   providers: [CredentialService, TrunkPlannerService, TrunksService],
 })
